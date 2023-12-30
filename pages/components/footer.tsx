0efd47e445dd4c71253d80a68764805790cf81db
FlavioHaber <134231585+FlavioHaber@users.noreply.github.com>
2023-12-30 15:52:32 -0300

    Add files via upload

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import * as footerStyles from "../styles/footer.module.css"
+import "../styles/footer.module.css"
 
 export default function Footer(props:any) {
     return (
-        <p className={footerStyles.footer}>
+        <p className="footer">
             {props.copyrightYear} Estudos de Jamstack - NextJS. Todos os direitos reservados.
         </p>
     )
